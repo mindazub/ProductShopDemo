62c3833db9144b5fe0a6eabdd359164f062c9d98
Mindaugas Ažubalis <mindaugas.azubalis@gmail.com>
2023-02-26 21:26:06 +0200

    Create Form is OK. Next -> Update

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ using (var scope = app.Services.CreateScope())
     if (context.Database.IsSqlServer() && !context.Products.Any())
     {
         context.Database.Migrate();
-        SeedData.Initialize(services, adminUser);
+        SeedData.Initialize(services);
     }
 }
 
